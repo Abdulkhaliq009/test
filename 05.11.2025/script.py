dfbd55f30b6733685d6fccc946ed1751e32cc5dd
Abdulkhaliq009 <abdulkhaliq24@icloud.com>
2025-11-05 15:23:58 +0100

    python updated

---
+++ 05.11.2025/script.py
@@ -0,0 +1,9 @@
+print ("Hello World")
+
+print ("what is yours")
+name = input ()
+print ("Hello , " + name + "!")
+greet = "welcome to my first project"
+print  (greet)
+age = ("how old are you ")
+age_input = input(age)
